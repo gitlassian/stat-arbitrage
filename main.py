87b0c944cb7699149531cd1befdeb7b5c35d0878
gitlassian <twinkkyarik@gmail.com>
2026-03-18 16:18:01 +0100

    fix: bug where bool var was absent and change of currencies

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 start_time = time.time_ns()
 
-ticker_symbol1 = "USDTRY=X"
+ticker_symbol1 = "GBPJPY=X"
 ticker_symbol2 = "EURJPY=X"
 
 color_pairs = [
@@ -22,11 +22,10 @@ fig = make_subplots(rows=1, cols=1, shared_xaxes=True, vertical_spacing=0.05)
 
 for ticker_symbol, row in zip(tickers_symbols, range(0, len(tickers_symbols))):
     ticker = yf.Ticker(ticker_symbol)
-    data = ticker.history(start="2026-01-18", end="2026-02-18", interval="5m")
+    data = ticker.history(start="2026-01-22", end="2026-01-23", interval="5m")
 
-    if begin_from_same_point:
-        start_point = Decimal(str(data.iloc[0]["Open"]))
-        print(start_point)
+    start_point = Decimal(str(data.iloc[0]["Open"]))
+    print(start_point)
 
     fig.add_trace(
         go.Candlestick(
